c9df51adb07398b97d26fb9cfc9ef6e49af0fdcb
Sergio Kevin Veliz Aldaz <skveliz@espol.edu.ec>
2020-08-14 08:35:04 -0500

    se agregaron ventanas 1 y 2

--- app/src/main/java/com/example/amst1/NavegationActivity.java
+++ app/src/main/java/com/example/amst1/NavegationActivity.java
@@ -10,12 +10,12 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity {
+public class NavegationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_navegation);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
