22417aec4a4e46f486a6a11fd40448baa5ef4bd1
Sergio Kevin Veliz Aldaz <skveliz@espol.edu.ec>
2020-08-15 00:26:14 -0500

    Se agregan dos librerias recycleview y cardview

--- app/src/main/java/com/example/amst1/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/amst1/ui/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,19 +19,31 @@ import com.example.amst1.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private LinearLayout linealLayout;
+    private EditText etBuscador;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        /*final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
+
+        linealLayout = (LinearLayout) root.findViewById(R.id.linealLayout);
+        etBuscador = (EditText) root.findViewById(R.id.etBuscador);
         return root;
     }
+
+
+
+    public void abrirVentana4(){
+
+    }
+
 }
